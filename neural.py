27bdd109c09191f1b91e615c374d6c35699d3561
likemango <40531806+likemango@users.noreply.github.com>
2022-04-04 17:50:30 +0800

    full project
    mario_net_xiaoyao.chkpt perform better than trained_mario.chkpt

---
+++ neural.py
@@ -0,0 +1,46 @@
+from torch import nn
+import copy
+
+class MarioNet(nn.Module):
+    '''mini cnn structure
+    input -> (conv2d + relu) x 3 -> flatten -> (dense + relu) x 2 -> output
+    '''
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        c, h, w = input_dim
+
+        if h != 84:
+            raise ValueError(f"Expecting input height: 84, got: {h}")
+        if w != 84:
+            raise ValueError(f"Expecting input width: 84, got: {w}")
+
+        self.commonLayer = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten()
+            # nn.Linear(3136, 512),
+            # nn.ReLU(),
+            # nn.Linear(512, output_dim)
+        )
+        
+        self.advantage = nn.Sequential(
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim)
+            )
+        
+        self.value = nn.Sequential(
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+            )
+
+    def forward(self, x):
+        x = self.commonLayer(x)
+        advantage = self.advantage(x)
+        value = self.value(x)
+        return advantage + value - advantage.mean()
